b4f3624435ea37a810c77f6ddd7eb20fd8750581
Mason Johnson <mason.johnson@medmutual.com>
2021-01-21 15:32:53 -0500

    part 1
    finished first steps

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : StandardGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookTypes.Ranked;
+        }
+    }
+}
