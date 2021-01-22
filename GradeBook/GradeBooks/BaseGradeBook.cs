7b36a3468563310809c108112664665f05fb4ac8
MXJK <mason.johnson@medmutual.com>
2021-01-22 09:44:25 -0500

    updated gradebook logic

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,10 +14,12 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool weighted)
         {
             Name = name;
+            IsWeighted = weighted;
             Students = new List<Student>();
         }
 
@@ -110,15 +112,30 @@ namespace GradeBook.GradeBooks
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    if (IsWeighted && (studentType.ToString() == "Honors" || studentType.ToString() == "DualEnrolled"))
+                        return 5;
+                    else
+                        return 4;
                 case 'B':
-                    return 3;
+                    if (IsWeighted && (studentType.ToString() == "Honors" || studentType.ToString() == "DualEnrolled"))
+                        return 4;
+                    else
+                        return 3;
                 case 'C':
-                    return 2;
+                    if (IsWeighted && (studentType.ToString() == "Honors" || studentType.ToString() == "DualEnrolled"))
+                        return 3;
+                    else
+                        return 2;
                 case 'D':
-                    return 1;
+                    if (IsWeighted && (studentType.ToString() == "Honors" || studentType.ToString() == "DualEnrolled"))
+                        return 2;
+                    else
+                        return 1;
                 case 'F':
-                    return 0;
+                    if (IsWeighted && (studentType.ToString() == "Honors" || studentType.ToString() == "DualEnrolled"))
+                        return 1;
+                    else
+                        return 0;
             }
             return 0;
         }
